eaae0e08119df239942fac46fe4b6d2271c4ea99
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-04-09 17:29:43 +0900

    updated xmmpipeline

--- newton/xmmpipeline.sh
+++ newton/xmmpipeline.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 #mv
-mv *EMOS1*ImagingEvts.ds	mos1.fits
-mv *EMOS2*ImagingEvts.ds	mos2.fits
-mv *EPN*ImagingEvts.ds		epn1.fits
+mv *EMOS1*ImagingEvts.ds	mos01.fits
+mv *EMOS2*ImagingEvts.ds	mos02.fits
+mv *EPN*ImagingEvts.ds		epn01.fits
 
 #Create Light Curve
-evselect table=mos1.fits withrateset=yes rateset=mos1.lc maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes expression="(PATTERN == 0) && (PI > 10000) && #XMMEA_EM"
-evselect table=mos2.fits withrateset=yes rateset=mos2.lc maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes expression="(PATTERN == 0) && (PI > 10000) && #XMMEA_EM"
-evselect table=epn1.fits withrateset=yes rateset=epn1.lc maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes expression="(PATTERN == 0) && (PI in [10000:12000] && #XMMEA_EP)"
+evselect table=mos01.fits withrateset=yes rateset=mos01.lc maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes expression="(PATTERN == 0) && (PI > 10000) && #XMMEA_EM"
+evselect table=mos02.fits withrateset=yes rateset=mos02.lc maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes expression="(PATTERN == 0) && (PI > 10000) && #XMMEA_EM"
+evselect table=epn01.fits withrateset=yes rateset=epn01.lc maketimecolumn=yes timecolumn=TIME timebinsize=100 makeratecolumn=yes expression="(PATTERN == 0) && (PI in [10000:12000] && #XMMEA_EP)"
 
 #Create Gti
-tabgtigen table=mos1.lc gtiset=mos1.gti timecolumn=TIME expression="(RATE <= 1)"
-tabgtigen table=mos2.lc gtiset=mos2.gti timecolumn=TIME expression="(RATE <= 1)"
-tabgtigen table=epn1.lc gtiset=epn1.gti timecolumn=TIME expression="(RATE <= 1)"
+tabgtigen table=mos01.lc gtiset=mos01.gti timecolumn=TIME expression="(RATE <= 1)"
+tabgtigen table=mos02.lc gtiset=mos02.gti timecolumn=TIME expression="(RATE <= 1)"
+tabgtigen table=epn01.lc gtiset=epn01.gti timecolumn=TIME expression="(RATE <= 1)"
 
 #Applying Time Filters
-evselect table=mos1.fits withfilteredset=yes expression="GTI(mos1.gti,TIME) && (PATTERN <= 12) && (PI in [200:12000]) && #XMMEA_EM" filteredset=mos1.ffits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes
-evselect table=mos2.fits withfilteredset=yes expression="GTI(mos2.gti,TIME) && (PATTERN <= 12) && (PI in [200:12000]) && #XMMEA_EM" filteredset=mos2.ffits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes
-evselect table=epn1.fits withfilteredset=yes expression="GTI(epn1.gti,TIME) && (PATTERN <= 12) && (PI in [200:12000]) && #XMMEA_EP" filteredset=epn1.ffits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes
+evselect table=mos01.fits withfilteredset=yes expression="GTI(mos1.gti,TIME) && (PATTERN <= 12) && (PI in [200:12000]) && #XMMEA_EM" filteredset=mos01.ffits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes
+evselect table=mos02.fits withfilteredset=yes expression="GTI(mos2.gti,TIME) && (PATTERN <= 12) && (PI in [200:12000]) && #XMMEA_EM" filteredset=mos02.ffits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes
+evselect table=epn01.fits withfilteredset=yes expression="GTI(epn1.gti,TIME) && (PATTERN <= 12) && (PI in [200:12000]) && #XMMEA_EP" filteredset=epn01.ffits filtertype=expression keepfilteroutput=yes updateexposure=yes filterexposure=yes
