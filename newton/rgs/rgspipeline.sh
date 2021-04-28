c2ff07745843a20d1b083cfbd2972c730fc7abae
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-04-28 18:18:53 +0900

    added rgsproducts

--- newton/rgs/rgspipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Reprocess RGS data
-rgsproc bkgcorrect=no orders="1 2" spectrumbinning=lambda withmlambdacolumn=yes
-
-
-#Rename
-mv *R1U*EVENLI* rgs01.fits
-mv *R2U*EVENLI* rgs02.fits
