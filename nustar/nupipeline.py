71d5206b00c556c0a27c4299f1edf0598ee38e3e
Atsushi Tanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2021-11-05 10:04:57 +0900

    Updated nupipeline

--- nustar/nupipeline.py
+++ nustar/nupipeline.py
@@ -7,5 +7,5 @@ def main():
     subprocess.call("nupipeline indir=../../{0:s} outdir=. saamode=optimized steminputs=nu{0:s} tentacle=yes".format(obsid), shell=True)
 
 
-if __name__="__main__":
+if __name__=="__main__":
     main()
