45ae8302bc950b6114d81bee5d8d49e13bd02e9b
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-05-06 18:29:53 +0900

    Changes to be committed: 	modified:   nupipeline.py

--- nustar/nupipeline.py
+++ nustar/nupipeline.py
@@ -1,11 +1,7 @@
 import subprocess
 
 
-def main():
+if __name__=="__main__":
     print("Please enter ObsID.")
     obsid = input()
-    subprocess.call("nupipeline indir=../../{0:s} outdir=. saamode=optimized steminputs=nu{0:s} tentacle=yes".format(obsid), shell=True)
-
-
-if __name__=="__main__":
-    main()
+    subprocess.run("nupipeline indir=../../{0:s} outdir=. saamode=optimized steminputs=nu{0:s} tentacle=yes".format(obsid), shell=True)
