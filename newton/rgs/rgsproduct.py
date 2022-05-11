1ccd14e6e491903a95c14c8a4328becdac990769
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-05-11 19:15:17 +0900

    Changes to be committed: 	modified:   rgsproduct.py

--- newton/rgs/rgsproduct.py
+++ newton/rgs/rgsproduct.py
@@ -13,7 +13,7 @@ if __name__=="__main__":
     subprocess.run("evselect  expression='(CCDNR==9)&&(REGION(rgs02src.fits:RGS2_BACKGROUND, M_LAMBDA, XDSP_CORR))' makeratecolumn=yes maketimecolumn=yes rateset=rgs02src.lc table=rgs02.fits timebinsize=100 withrateset=yes", shell=True)
     subprocess.run("tabgtigen expression='(RATE<=0.2)' gtiset=rgs01src.gti table=rgs01src.lc", shell=True)
     subprocess.run("tabgtigen expression='(RATE<=0.2)' gtiset=rgs02src.gti table=rgs02src.lc", shell=True)
-    subprocess.run("rgsproc auxgtitables=rgs01src.gti bkgcorrect=no entrystage=3:filter finalstage=5:fluxing orders=1 withmlambdacolumn=yes", shell=True)
+    subprocess.run("rgsproc auxgtitables=rgs01src.gti bkgcorrect=no orders=1 spectrumbinning=lambda withmlambdacolumn=yes", shell=True)
 
     subprocess.run("mv *R1S*EVENLI*FIT  rgs01.fits",    shell=True)
     subprocess.run("mv *R2S*EVENLI*FIT  rgs02.fits",    shell=True)
