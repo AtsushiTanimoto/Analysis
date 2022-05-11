641adffd77a46aefe2d729b2f5e05f61d07f0c9f
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-05-11 19:41:59 +0900

    Changes to be committed: 	modified:   rgsproduct.py

--- newton/rgs/rgsproduct.py
+++ newton/rgs/rgsproduct.py
@@ -26,4 +26,4 @@ if __name__=="__main__":
     
     subprocess.run("rgsrmfgen emin=0.5 emax=2.5 evlist=rgs01.fits rmfset=rgs01.rmf rows=5000 spectrumset=rgs01src.pha srclist=rgs01src.fits", shell=True)
     subprocess.run("rgsrmfgen emin=0.5 emax=2.5 evlist=rgs02.fits rmfset=rgs02.rmf rows=5000 spectrumset=rgs02src.pha srclist=rgs02src.fits", shell=True)
-    subprocess.run("rgscombine bkg='rgs01bkg.pha rgs02bkg.pha' filebkg='rgs0001bkg.pha' filepha='rgs0001src.pha' filermf='rgs0001src.rmf' pha='rgs01src.pha rgs02src.pha' rmf='rgs01.rmf rgs02.rmf' rmfgrid=5000", shell=True)
+    subprocess.run("rgscombine bkg='rgs01bkg.pha rgs02bkg.pha' filebkg=rgs0001bkg.pha filepha=rgs0001src.pha filermf=rgs0001src.rmf pha='rgs01src.pha rgs02src.pha' rmf='rgs01.rmf rgs02.rmf' rmfgrid=5000", shell=True)
