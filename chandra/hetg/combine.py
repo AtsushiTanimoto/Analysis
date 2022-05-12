3fd9bc2bf573884f765952ca3c38a3c7cd5f84aa
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-05-12 16:47:21 +0900

    Changes to be committed: 	deleted:    heg/combine.py 	renamed:    heg/arf.txt -> hetg/arf.txt 	new file:   hetg/combine.py 	renamed:    heg/pha.txt -> hetg/pha.txt 	renamed:    heg/rmf.txt -> hetg/rmf.txt

---
+++ chandra/hetg/combine.py
@@ -0,0 +1,6 @@
+import subprocess
+
+
+if __name__=="__main__":
+    filename = input
+    subprocess.run("combine_grating_spectra add_plusminus=yes arf=@arf.txt infile=@pha.txt out={0:s} rmf=@rmf.txt".format(filename), shell=True)
