c06e85e03a45fe762b8f2588c658bb83bde7dfe5
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-05-12 16:52:21 +0900

    Changes to be committed: 	modified:   hetgprodcuts.py

--- chandra/hetg/hetgprodcuts.py
+++ chandra/hetg/hetgprodcuts.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 if __name__=="__main__":
-    filename = input
+    filename = input()
     subprocess.run("combine_grating_spectra add_plusminus=yes arf=@arf.txt infile=@pha.txt out={0:s} rmf=@rmf.txt".format(filename), shell=True)
