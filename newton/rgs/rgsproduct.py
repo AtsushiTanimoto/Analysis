ee3cbbd3fec360e028400c53f271cb0df3d40e5d
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-05-16 18:15:06 +0900

    Changes to be committed: 	modified:   rgsproduct.py

--- newton/rgs/rgsproduct.py
+++ newton/rgs/rgsproduct.py
@@ -24,6 +24,6 @@ if __name__=="__main__":
     subprocess.run("mv *R1S*SRSPEC1001* rgs01src.pha",  shell=True)
     subprocess.run("mv *R2S*SRSPEC1001* rgs02src.pha",  shell=True)
     
-    subprocess.run("rgsrmfgen emin=0.5 emax=2.5 evlist=rgs01.fits rmfset=rgs01.rmf rows=5000 spectrumset=rgs01src.pha srclist=rgs01src.fits", shell=True)
-    subprocess.run("rgsrmfgen emin=0.5 emax=2.5 evlist=rgs02.fits rmfset=rgs02.rmf rows=5000 spectrumset=rgs02src.pha srclist=rgs02src.fits", shell=True)
-    subprocess.run("rgscombine bkg='rgs01bkg.pha rgs02bkg.pha' filebkg=rgs0001bkg.pha filepha=rgs0001src.pha filermf=rgs0001src.rmf pha='rgs01src.pha rgs02src.pha' rmf='rgs01.rmf rgs02.rmf' rmfgrid=5000", shell=True)
+    subprocess.run("rgsrmfgen emin=0.5 emax=2.5 evlist=rgs01.fits rmfset=rgs01src.rmf rows=5000 spectrumset=rgs01src.pha srclist=rgs01src.fits", shell=True)
+    subprocess.run("rgsrmfgen emin=0.5 emax=2.5 evlist=rgs02.fits rmfset=rgs02src.rmf rows=5000 spectrumset=rgs02src.pha srclist=rgs02src.fits", shell=True)
+    subprocess.run("rgscombine bkg='rgs01bkg.pha rgs02bkg.pha' filebkg=rgs0001bkg.pha filepha=rgs0001src.pha filermf=rgs0001src.rmf pha='rgs01src.pha rgs02src.pha' rmf='rgs01src.rmf rgs02src.rmf' rmfgrid=5000", shell=True)
