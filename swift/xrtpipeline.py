ad87b5e2185a554f5c8cd15532e26823e66f59d2
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-08-25 16:05:18 +0900

    Changes to be committed: 	modified:   swift/xrtpipeline.py 	deleted:    swift/xrtpipeline.sh

--- swift/xrtpipeline.py
+++ swift/xrtpipeline.py
@@ -3,5 +3,5 @@ import subprocess
 
 
 if __name__=="__main__":
-    obsid = os.listdir("../../../")[0]
-    subprocess.run("xrtpipeline indir=../../{0:s} outdir=. steminputs=sw{0:s} srcra=OBJECT srcdec=OBJECT plotdevice=ps".format(obsid), shell=True)
+    obsid = os.listdir("../swift/")[0]
+    subprocess.run("xrtpipeline indir=../swift/{0:s} outdir=. steminputs=sw{0:s} srcra=OBJECT srcdec=OBJECT plotdevice=ps".format(obsid), shell=True)
--- swift/xrtpipeline.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-number=`ls ../swift`
-xrtpipeline indir=../swift/$number outdir=. plotdevice="ps" srcra=OBJECT srcdec=OBJECT steminputs=sw$number stemoutputs=DEFAULT  
