e8958eaa53ae7dec6d1043acabab90fa71db034d
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-09-08 09:52:03 +0900

    Changes to be committed: 	modified:   chandra/acis/acisproducts.py

--- chandra/acis/acisproducts.py
+++ chandra/acis/acisproducts.py
@@ -4,7 +4,7 @@ import subprocess
 
 if __name__=="__main__":
     fin = glob.glob("*evt2.fits")[0]
-    subprocess.run("specextract infile='{0:s}[sky=region(src.reg)]' outroot=acs bkgfile='{0:s}[sky=region(bkg.reg)]'".format(fin), shell=True)
+    subprocess.run("specextract infile='{0:s}[sky=region(sour.reg)]' outroot=acs bkgfile='{0:s}[sky=region(back.reg)]'".format(fin), shell=True)
     subprocess.run("mv acs_bkg.pi acsbkg.pha", shell=True)
     subprocess.run("mv acs.arf    acssrc.arf", shell=True)
     subprocess.run("mv acs.pi     acssrc.pha", shell=True)
