b4a1ee0455795afd79112fb3f0fc090cdf11626b
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-09-08 10:25:16 +0900

    Changes to be committed: 	modified:   swift/xrtproducts.py

--- swift/xrtproducts.py
+++ swift/xrtproducts.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
     exfile  = glob.glob("*xpcw3po_ex.img")[0]
     hkfile  = glob.glob("*.hk")[0]
     infile  = glob.glob("*xpcw3po_cl.evt")[0]
-    subprocess.run("xrtproducts infile={0:s} regionfile=src.reg bkgextract=yes bkgregionfile=bkg.reg outdir=. expofile={1:s} correctlc=no clobber=yes".format(infile, exfile), shell=True)
+    subprocess.run("xrtproducts infile={0:s} regionfile=sour.reg bkgextract=yes bkgregionfile=back.reg outdir=. expofile={1:s} correctlc=no clobber=yes".format(infile, exfile), shell=True)
     subprocess.run("mv *xpcw3pobkg.pha xrtbkg.pha", shell=True)
     subprocess.run("mv *xpcw3posr.pha  xrtsrc.pha", shell=True)
     subprocess.run("mv *xpcw3posr.arf  xrtsrc.arf", shell=True)
