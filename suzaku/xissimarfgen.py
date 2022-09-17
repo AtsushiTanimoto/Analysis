9f2f4b2bd4b316df244a3d47483cf5c579948098
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-09-17 18:48:28 +0900

    Changes to be committed: 	renamed:    chandra/acis/acisfit.py -> chandra/acis/acisadd.py 	modified:   suzaku/xissimarfgen.py

--- suzaku/xissimarfgen.py
+++ suzaku/xissimarfgen.py
@@ -18,11 +18,11 @@ if __name__=="__main__":
                 soux = data[ 7:16]
                 souy = data[17:26]
     
-    if len(xis0gti)==1:
+    if 1<=len(xis0gti):
         subprocess.run("xissimarfgen instrume=XIS0 rmffile=xis0src.rmf estepfile=dense source_mode=SKYXY source_x={0:s} source_y={1:s} num_region=1 region_mode=SKYREG regfile1=sour.reg arffile1=xis0src.arf detmask=none limit_mode=NUM_PHOTON num_photon=100000 gtifile={2:s} pointing=AUTO attitude={3:s} phafile=xis0src.pha".format(soux, souy, xis0gti[0], xis0att), shell=True)
-    if len(xis1gti)==1:
+    if 1<=len(xis1gti):
         subprocess.run("xissimarfgen instrume=XIS1 rmffile=xis1src.rmf estepfile=dense source_mode=SKYXY source_x={0:s} source_y={1:s} num_region=1 region_mode=SKYREG regfile1=sour.reg arffile1=xis1src.arf detmask=none limit_mode=NUM_PHOTON num_photon=100000 gtifile={2:s} pointing=AUTO attitude={3:s} phafile=xis1src.pha".format(soux, souy, xis1gti[0], xis1att), shell=True)
-    if len(xis2gti)==1:
+    if 1<=len(xis2gti):
         subprocess.run("xissimarfgen instrume=XIS2 rmffile=xis2src.rmf estepfile=dense source_mode=SKYXY source_x={0:s} source_y={1:s} num_region=1 region_mode=SKYREG regfile1=sour.reg arffile1=xis2src.arf detmask=none limit_mode=NUM_PHOTON num_photon=100000 gtifile={2:s} pointing=AUTO attitude={3:s} phafile=xis2src.pha".format(soux, souy, xis2gti[0], xis2att), shell=True)
-    if len(xis3gti)==1:
+    if 1<=len(xis3gti):
         subprocess.run("xissimarfgen instrume=XIS3 rmffile=xis3src.rmf estepfile=dense source_mode=SKYXY source_x={0:s} source_y={1:s} num_region=1 region_mode=SKYREG regfile1=sour.reg arffile1=xis3src.arf detmask=none limit_mode=NUM_PHOTON num_photon=100000 gtifile={2:s} pointing=AUTO attitude={3:s} phafile=xis3src.pha".format(soux, souy, xis3gti[0], xis3att), shell=True)
