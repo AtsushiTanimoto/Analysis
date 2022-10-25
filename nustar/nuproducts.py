14340acb71f10f1f23c52762b6fc60d531dd0483
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-10-25 18:31:22 +0900

    Changes to be committed: 	renamed:    nustar/fpmfit.py -> nustar/fpmadd.py 	modified:   nustar/nuproducts.py

--- nustar/nuproducts.py
+++ nustar/nuproducts.py
@@ -6,8 +6,8 @@ if __name__=="__main__":
     obsid       = os.listdir("../../")[0]
     steminputs  = "nu{0:s}".format(obsid)
 
-    subprocess.run("nuproducts indir=. instrument=FPMA steminputs={0:s} outdir=. srcregionfile=src.reg bkgextract=yes bkgregionfile=bkg.reg".format(steminputs), shell=True)
-    subprocess.run("nuproducts indir=. instrument=FPMB steminputs={0:s} outdir=. srcregionfile=src.reg bkgextract=yes bkgregionfile=bkg.reg".format(steminputs), shell=True)    
+    subprocess.run("nuproducts indir=. instrument=FPMA steminputs={0:s} outdir=. srcregionfile=sour.reg bkgextract=yes bkgregionfile=back.reg".format(steminputs), shell=True)
+    subprocess.run("nuproducts indir=. instrument=FPMB steminputs={0:s} outdir=. srcregionfile=sour.reg bkgextract=yes bkgregionfile=back.reg".format(steminputs), shell=True)    
     subprocess.run("mv nu*A01_bk.pha fpmabkg.pha", shell=True)
     subprocess.run("mv nu*A01_sr.arf fpmasrc.arf", shell=True)
     subprocess.run("mv nu*A01_sr.pha fpmasrc.pha", shell=True)
