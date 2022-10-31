1a88836cba5d525727d3c83e013a681f3afea032
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-10-31 17:00:05 +0900

    Changes to be committed: 	modified:   nustar/nupipeline.py

--- nustar/nupipeline.py
+++ nustar/nupipeline.py
@@ -3,8 +3,8 @@ import os
 
 
 if __name__=="__main__":
-    nupipeline  = heasoftpy.HSPTask("nupipeline")
     obsid       = os.listdir("../../")[0]
     indir       = "../../{0:s}".format(obsid)
     steminputs  = "nu{0:s}".format(obsid)
+    nupipeline  = heasoftpy.HSPTask("nupipeline")
     nupipeline(indir=indir, steminputs=steminputs, outdir=".", saamode="optimized", tentacle="yes", noprompt=True, verbose=True)
