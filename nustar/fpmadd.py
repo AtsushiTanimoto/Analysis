76dc8017a2923bbbffd80a019d8c9cbaa5a22a87
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-10-31 17:09:30 +0900

    Changes to be committed: 	modified:   nustar/fpmadd.py

--- nustar/fpmadd.py
+++ nustar/fpmadd.py
@@ -1,12 +1,9 @@
 import subprocess
 
 
-def main():
+if __name__=="__main__":
     subprocess.call("addascaspec fpm.dat fpmsrc.pha fpmsrc.rsp fpmbkg.pha", shell=True)
     subprocess.call("mv fpmbkg.pha ../../../analysis", shell=True)
     subprocess.call("mv fpmsrc.pha ../../../analysis", shell=True)
     subprocess.call("mv fpmsrc.rsp ../../../analysis", shell=True)
 
-
-if __name__=="__main__":
-    main()
