c963cb4bf2c9b416d8f3ac38653b501bc482bac6
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-11-16 16:40:41 +0900

    Changes to be committed: 	modified:   suzaku/xispipeline.py

--- suzaku/xispipeline.py
+++ suzaku/xispipeline.py
@@ -1,7 +1,8 @@
+import heasoftpy
 import os
-import subprocess
 
 
 if __name__=="__main__":
-    obsid   = os.listdir("../suzaku/")[0]
-    subprocess.run("aepipeline indir=../suzaku/{0:s} outdir=. steminputs={0:s} entry_stage=1 exit_stage=2 instrument=XIS clobber=yes".format(obsid), shell=True)
+    obsid       = os.listdir("../suzaku/")[0]
+    aepipeline  = heasoftpy.HSPTask("aepipeline")
+    aepipeline(indir="../suzaku/{0:s}".format(obsid), outdir=".", steminputs="{0:s}".format(obsid), entry_stage=1, exit_stage=2, instrument="XIS", clobber="yes", noprompt=True, verbose=True)
