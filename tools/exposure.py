71401f9ea3a672b17b6ac28fc3d6967ae82c0674
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-11-17 11:01:19 +0900

    modified:   tools/exposure.py

--- tools/exposure.py
+++ tools/exposure.py
@@ -5,7 +5,7 @@ import os
 if __name__=="__main__":
     if os.path.exists("acssrc.pha"):
         with astropy.io.fits.open("acssrc.pha") as fin:
-            print("Exposure of Chandra/ACIS = {0:02d} ksec".format(round(fin[0].header["EXPOSURE"]/1000)))
+            print("Exposure of Chandra/ACIS = {0:02d} ksec".format(round(fin[1].header["EXPOSURE"]/1000)))
     
     if os.path.exists("epnsrc.pha"):
         with astropy.io.fits.open("epnsrc.pha") as fin:
