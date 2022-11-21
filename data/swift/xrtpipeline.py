e4eb9b2846cc58770a55640b7087b50f429e1212
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2022-11-21 13:50:43 +0900

    modified:   data/swift/xrtpipeline.py

--- data/swift/xrtpipeline.py
+++ data/swift/xrtpipeline.py
@@ -1,7 +1,8 @@
+import heasoftpy
 import os
-import subprocess
 
 
 if __name__=="__main__":
     obsid = os.listdir("../swift/")[0]
-    subprocess.run("xrtpipeline indir=../swift/{0:s} outdir=. steminputs=sw{0:s} srcra=OBJECT srcdec=OBJECT plotdevice=ps".format(obsid), shell=True)
+    xrtpipeline = heasoftpy.HSPTask("xrtpipeline")
+    xrtpipeline(indir="../swift/{0:s}".format(obsid), outdir=".", steminputs="sw{0:s}".format(obsid), srcra="OBJECT", srcdec="OBJECT", plotdevice="ps", noprompt=True, verbose=True)
