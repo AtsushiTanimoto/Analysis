0eaa585e2e4c2ff89fa105d4482eac9ad9adb8e1
AtsushiTanimoto <atsushi.tanimoto@phys.s.u-tokyo.ac.jp>
2024-03-18 18:56:50 +0900

    Changes to be committed: 	new file:   data/chandra/hetg/hetgarf.txt 	new file:   data/chandra/hetg/hetgpha.txt 	modified:   data/chandra/hetg/hetgproducts.py 	deleted:    data/chandra/hetg/hetgreprocess.py 	new file:   data/chandra/hetg/hetgrmf.txt

--- data/chandra/hetg/hetgproducts.py
+++ data/chandra/hetg/hetgproducts.py
@@ -2,5 +2,6 @@ import subprocess
 
 
 if __name__=="__main__":
+    print("Please enter the output filename")
     filename = input()
     subprocess.run("combine_grating_spectra add_plusminus=yes arf=@hetgarf.txt infile=@hetgpha.txt out={0:s} rmf=@hetgrmf.txt".format(filename), shell=True)
--- data/chandra/hetg/hetgreprocess.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-
-if __name__=="__main__":
-    subprocess.run("chandra_repro indir=../ outdir=../analysis", shell=True)
