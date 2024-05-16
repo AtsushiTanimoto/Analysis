5bdada5556e392f5156a05de9b61b65c6788a826
Atsushi Tanimoto <ats141592@gmail.com>
2024-05-16 16:40:13 +0900

    Updated fpmproducts

--- data/nustar/fpmproducts.py
+++ data/nustar/fpmproducts.py
@@ -7,15 +7,6 @@ if __name__=="__main__":
     obsid           = os.listdir("../../")[0]
     steminputs      = "nu{0:s}".format(obsid)
     nuproducts01    = heasoftpy.HSPTask("nuproducts")
-    nuproducts02  = heasoftpy.HSPTask("nuproducts")
-    nuproducts01({"indir":".", "instrument":"FPMA", "steminputs":"{0:s}".format(steminputs), "outdir":".", "srcregionfile":"sour.reg", "bkgextract":"yes", "bkgregionfile":"back.reg"}, noprompt=True, verbose=True)
-    nuproducts02({"indir":".", "instrument":"FPMB", "steminputs":"{0:s}".format(steminputs), "outdir":".", "srcregionfile":"sour.reg", "bkgextract":"yes", "bkgregionfile":"back.reg"}, noprompt=True, verbose=True)
-    
-    subprocess.run("mv nu*A01_bk.pha fpmabkg.pha", shell=True)
-    subprocess.run("mv nu*A01_sr.arf fpmasrc.arf", shell=True)
-    subprocess.run("mv nu*A01_sr.pha fpmasrc.pha", shell=True)
-    subprocess.run("mv nu*A01_sr.rmf fpmasrc.rmf", shell=True)
-    subprocess.run("mv nu*B01_bk.pha fpmbbkg.pha", shell=True)
-    subprocess.run("mv nu*B01_sr.arf fpmbsrc.arf", shell=True)
-    subprocess.run("mv nu*B01_sr.pha fpmbsrc.pha", shell=True)
-    subprocess.run("mv nu*B01_sr.rmf fpmbsrc.rmf", shell=True)
+    nuproducts02    = heasoftpy.HSPTask("nuproducts")
+    nuproducts01({"indir":".", "instrument":"FPMA", "steminputs":"{0:s}".format(steminputs), "outdir":".", "srcregionfile":"sour.reg", "bkgregionfile":"back.reg"}, noprompt=True, verbose=True)
+    nuproducts02({"indir":".", "instrument":"FPMB", "steminputs":"{0:s}".format(steminputs), "outdir":".", "srcregionfile":"sour.reg", "bkgregionfile":"back.reg"}, noprompt=True, verbose=True)
