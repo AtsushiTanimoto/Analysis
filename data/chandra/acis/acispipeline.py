dea2d796e9469c3fa4c44d0350ef37f151cbb60a
Atsushi Tanimoto <ats141592@gmail.com>
2024-07-30 17:06:49 +0900

    Changes to be committed: 	modified:   data/chandra/acis/acispipeline.py

--- data/chandra/acis/acispipeline.py
+++ data/chandra/acis/acispipeline.py
@@ -1,7 +1,7 @@
-import glob
 import subprocess
 
 
 if __name__=="__main__":
-    obsid = glob.glob("../../*")[0]
+    print("Please enter the obsid.")
+    obsid = input()
     subprocess.run("chandra_repro {0:s} outdir=.".format(obsid), shell=True)
